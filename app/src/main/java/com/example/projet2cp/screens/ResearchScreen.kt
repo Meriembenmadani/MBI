780271f90daf2cc07b841ad5f8e54f6c2c89bd55
Meriembenmadani <m_benmadani@estin.dz>
2024-04-03 23:57:34 +0100

    Implemented data layer - with uiEvents and ViewModel

--- app/src/main/java/com/example/projet2cp/screens/ResearchScreen.kt
+++ app/src/main/java/com/example/projet2cp/screens/ResearchScreen.kt
@@ -1,23 +1,36 @@
 package com.example.projet2cp.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -27,17 +40,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.projet2cp.R
 import com.example.projet2cp.ui.theme.Black
 import com.example.projet2cp.ui.theme.MyBleu
@@ -80,6 +96,9 @@ fun ResearchScreen() {
 fun ResearchSection(screenHeight: Dp,screenWidth:Dp) {
     val bgColor =  if (isSystemInDarkTheme()) Black else Color.White
     val uiColor = if (isSystemInDarkTheme()) MyPurple else MyBleu
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
 
     Surface(
         modifier = Modifier
@@ -91,9 +110,11 @@ fun ResearchSection(screenHeight: Dp,screenWidth:Dp) {
     ) {
         Column(modifier = Modifier
             .fillMaxHeight()
-            .padding(top = screenHeight * 0.05f,
+            .padding(
+                top = screenHeight * 0.05f,
                 start = screenWidth * 0.05f,
-                end = screenWidth * 0.05f,)
+                end = screenWidth * 0.05f,
+            )
             ,
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top,
@@ -104,18 +125,135 @@ fun ResearchSection(screenHeight: Dp,screenWidth:Dp) {
                 fontSize = 18.sp,
                 color = uiColor
             )
-            TextFieldR(modifier = Modifier.fillMaxWidth(),"Language")
+            TextFieldR(
+                modifier = Modifier.fillMaxWidth(),
+                "Language",
+                onButtonClick = {buttonClickedState.value= ! buttonClickedState.value}
+            )
+
+            if (buttonClickedState.value){
+                Box(
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    Dialog(
+                        onDismissRequest = {
+                            buttonClickedState.value=false
+                        }) {
+                       Languages(screenHeight)
+                    }
+                }
+
+            } else{
+                Box {}
+            }
         }
 
     }
 
 }
 
+@Preview
+@Composable
+fun Languages(screenHeight:Dp ) {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
+    val uiColor = if (isSystemInDarkTheme()) MyPurple else MyBleu
+  Column (){
+
+
+    Box (modifier= Modifier
+        .fillMaxSize()
+        .fillMaxWidth()
+        .padding(bottom = screenHeight * 0.07f, top = 5.dp, start = 5.dp, end = 5.dp)){
+        Surface(modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+            border =  BorderStroke(width = 1.dp , color = uiColor),
+            color = if (isSystemInDarkTheme()) Black else Color.White
+        ) {
+            Language(data = listOf("English A0", "English A1" , "English A2"))
+
+        }
+
+    }
+  }
+
+}
+
+@Preview
+@Composable
+fun Language(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth()
+                    .height(150.dp),
+                shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+                colors = CardDefaults.cardColors(containerColor =  if (isSystemInDarkTheme()) Color(0xFFB6B6B6) else Color(0xFFE7E0EC)),
+
+
+                ) {
+                Row(modifier= Modifier
+                    .padding(10.dp)
+                    .padding(16.dp)) {
+                    CreateImageProject()
+                    Column(modifier = Modifier
+                        .padding(7.dp)
+                    ) {
+                        Text(text = item,
+                            fontFamily = FontFamily(listOf(Font(R.font.poppins_semi_bold))),
+                            fontSize = 20.sp,
+                            color = Black)
+                        Text(
+                            text = "You have done a great job",
+                            modifier = Modifier.padding(2.dp),
+                            fontFamily = FontFamily(listOf(Font(R.font.poppins_regular))),
+                            fontSize = 11.sp,
+                            color =  Black
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CreateImageFlag(modifier: Modifier= Modifier) {
+    val uiColor = if (isSystemInDarkTheme()) MyPurple else MyBleu
+
+    Surface(
+        modifier = Modifier
+            .size(90.dp)
+            .padding(2.dp),
+        shape = RoundedCornerShape(12.dp),
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.certificate), contentDescription = "profile image",
+            modifier = modifier.size(5.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun TextFieldR(
        modifier: Modifier = Modifier,
         label: String,
+       onButtonClick: () -> Unit
 
     ) {
 
@@ -172,7 +310,7 @@ fun TextFieldR(
 
                 ),
             trailingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { onButtonClick }) {
                     Image(
                         painter = if (isSystemInDarkTheme()) painterResource(id = R.drawable.rd) else painterResource(id = R.drawable.r),
                         contentDescription = "Icon Description"
